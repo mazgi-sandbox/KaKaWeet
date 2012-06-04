f9a117553bfe019c2ca9c69d3bfeb605c3c10eb2
MATSUKI Hidenori <hidenori.matsuki@drecom.co.jp>
2012-06-04 20:05:15 +0900

    create TweetTestDrive project

---
+++ iOS/TweetTestDrive/TweetTestDrive/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TweetTestDrive
+//
+//  Created by Hidenori MATSUKI on 12/06/04.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
